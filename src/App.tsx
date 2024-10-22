ec0660f9558a6854665c1a3c3e30f83be3a1c01e
9182092380 <pothulasharath452@gmail.com>
2024-10-22 11:24:48 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+// src/App.tsx
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AddEmployeeWithNavigate from './components/AddEmployee';
+import EditEmployeeWithParams from './components/EditEmployee';
+import EmployeeList from './components/EmployeeList';
+import { Employee } from './components/Employee';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+interface AppState {
+  employees: Employee[];
+}
+
+class App extends Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      employees: [],
+    };
+  }
+
+  addEmployee = (employee: Employee) => {
+    this.setState((prevState) => ({
+      employees: [...prevState.employees, employee],
+    }));
+  };
+
+  editEmployee = (editedEmployee: Employee) => {
+    const updatedEmployees = this.state.employees.map((emp) =>
+      emp.id === editedEmployee.id ? editedEmployee : emp
+    );
+    this.setState({ employees: updatedEmployees });
+  };
+
+  deleteEmployee = (id: number) => {
+    const filteredEmployees = this.state.employees.filter((emp) => emp.id !== id);
+    this.setState({ employees: filteredEmployees });
+  };
+
+  render() {
+    return (
+      <Router>
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<AddEmployeeWithNavigate onAddEmployee={this.addEmployee} />} />
+            <Route path="/employees" element={<EmployeeList employees={this.state.employees} onDeleteEmployee={this.deleteEmployee} />} />
+            <Route path="/edit/:id" element={<EditEmployeeWithParams employees={this.state.employees} onEditEmployee={this.editEmployee} />} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
